efcc1186caa53726c8717d375daddcd17b9a6301
Kevin Dunn <kevindunn31@gmail.com>
2022-02-25 13:59:45 -0600

    Needs some Work

---
+++ about.js
@@ -0,0 +1,31 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('Form has been submitted');
+}
+
+
+let form = document.querySelector('form#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+let 
+
+
+function bigImg(x) {
+	x.style.height = "500px";
+	x.style.width = "400px";
+
+}
+bigImg.addEventListener(mouseover,'');
+
+function normalImg(x){
+	x.style.height = "460px";
+	x.style.width = "345px";
+}
+normalImg.addEventListner(mouseover,'');
+
---
+++ profile.js
@@ -0,0 +1,28 @@
+const favoriteColorBtn = document.querySelector("#color");
+const favoritePlaceBtn = document.querySelector("#place");
+const favoriteRitualBtn = document.querySelector("#ritual)");
+
+function color (event){
+    event.preventDefault();
+    alert('Blue');
+}
+    favoriteColorBtn.addEventListener('click', color;
+
+    function place (event){
+        event.preventDefault();
+        alert('Napa Valley')
+    }
+
+    favoritePlaceBtn.addEventListener('click', place);
+
+    function ritual (event){
+        event.preventDefault();
+        alert('Drink water right away when I wake up');
+    }
+
+    favoriteRitualBtn.addEventListener('click', ritual);
+
+
+
+
+
